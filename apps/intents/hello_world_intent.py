092abf52185291736657f2711cc54faec3c6c59a
Arkadiusz Buras <macbury@gmail.com>
2018-06-15 20:57:04 +0200

    Added handling cuvvete

--- apps/intents/hello_world_intent.py
+++
@@ -1,9 +0,0 @@
-from intent_handler import IntentHandler
-
-class HelloWorldIntent(IntentHandler):
-
-  def name(self):
-    return 'hello.world'
-
-  def call(self, data):
-    return "Hello world!"
